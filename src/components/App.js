2c26d865f9b500fdfff19c56707c4ad917567621
Yegor Sytnyk <yegor.sytnyk@gmail.com>
2016-10-14 16:27:48 +1000

    delete movie functionality

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import noMovie from '../media/no-movie.png';
 import '../styles/App.css';
 import moviesService from '../movieService';
+import {Button, Glyphicon} from 'react-bootstrap';
+import toastr from 'toastr';
 
 
 class App extends Component {
@@ -13,6 +15,7 @@ class App extends Component {
         };
 
         this.loadData = this.loadData.bind(this);
+        this.deleteMovie = this.deleteMovie.bind(this);
     }
 
     loadData() {
@@ -21,6 +24,14 @@ class App extends Component {
                 this.setState({
                     movies: data
                 })
+            });
+    }
+
+    deleteMovie(id) {
+        moviesService.deleteMovie(id)
+            .then(() => {
+                toastr.success('Movie was deleted');
+                this.loadData();
             });
     }
 
@@ -41,16 +52,16 @@ class App extends Component {
     }
 
     renderMoviesList(movies) {
-        if (!movies.length) return (
+        if (!movies || !movies.length) return (
             <div>No movies loaded.</div>
         );
 
         return (
             <table className="container">
                 <tbody>
-                    {
-                        movies.map(movie => this.renderMovie(movie))
-                    }
+                {
+                    movies.map(movie => this.renderMovie(movie))
+                }
                 </tbody>
             </table>
         );
@@ -64,12 +75,15 @@ class App extends Component {
                 <td className="number">{movie.id}.</td>
 
                 <td className="image">
-                    <img width={70} height={96} src={posterUrl} title={movie.title} alt={movie.title} />
+                    <img width={70} height={96} src={posterUrl} title={movie.title} alt={movie.title}/>
                 </td>
 
                 <td className="title">
                     <a>{movie.title}</a>
                     <span> ({movie.year})</span>
+                    <br/>
+                    <Button><Glyphicon glyph="edit"/></Button>
+                    <Button onClick={() => this.deleteMovie(movie.id)}><Glyphicon glyph="remove"/></Button>
                     <br/>
                     <span className="plot">{movie.plot}</span>
                     <br/>
--- src/movieService.js
+++ src/movieService.js
@@ -1,17 +1,34 @@
 import toastr from 'toastr';
 
 export default {
-    getMovies
+    getMovies,
+    deleteMovie
 };
 
 function getMovies() {
     return fetch('/movies')
         .then((response) => {
-            if (!response.ok) throw new Error(`Invalid HTTP response status ${response.status}`);
+            checkStatus(response);
 
             return response.json();
         })
         .catch((err) => {
             toastr.error(err);
         });
+}
+
+function deleteMovie(id) {
+    return fetch(`/movies/${id}`, {
+            method: 'DELETE'
+        })
+        .then((response) => {
+            checkStatus(response);
+        })
+        .catch((err) => {
+            toastr.error(err);
+        });
+}
+
+function checkStatus(response) {
+    if (!response.ok) throw new Error(`Invalid HTTP response status ${response.status}`);
 }
