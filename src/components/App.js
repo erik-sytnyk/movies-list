3dc7161b1d5cb27fa03b51a2f2fa31889e6bd9e1
Yegor Sytnyk <yegor.sytnyk@gmail.com>
2016-10-14 23:32:41 +1000

    delete confirmation

--- src/components/App.js
+++ src/components/App.js
@@ -4,18 +4,19 @@ import '../styles/App.css';
 import moviesService from '../movieService';
 import {Button, Glyphicon} from 'react-bootstrap';
 import toastr from 'toastr';
-
+import Confirm from './Confirm';
+import autoBind from 'react-autobind';
 
 class App extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            movies: []
+            movies: [],
+            movieToDeleteId: null
         };
 
-        this.loadData = this.loadData.bind(this);
-        this.deleteMovie = this.deleteMovie.bind(this);
+        autoBind(this);
     }
 
     loadData() {
@@ -27,11 +28,16 @@ class App extends Component {
             });
     }
 
-    deleteMovie(id) {
-        moviesService.deleteMovie(id)
+    deleteMovie() {
+        moviesService.deleteMovie(this.state.movieToDeleteId)
             .then(() => {
                 toastr.success('Movie was deleted');
+
                 this.loadData();
+
+                this.setState({
+                    movieToDeleteId: null
+                });
             });
     }
 
@@ -56,6 +62,8 @@ class App extends Component {
             <div>No movies loaded.</div>
         );
 
+        let deleteConfirmVisible = this.state.movieToDeleteId ? true: false;
+
         return (
             <div className="container">
                 <div id="movie-list">
@@ -63,8 +71,21 @@ class App extends Component {
                         movies.map(movie => this.renderMovie(movie))
                     }
                 </div>
+                <Confirm visible={deleteConfirmVisible} action={this.deleteMovie} close={this.cancelDeleteMovie}/>
             </div>
         );
+    }
+
+    confirmDeleteMovie(id) {
+        this.setState({
+            movieToDeleteId: id
+        });
+    }
+
+    cancelDeleteMovie() {
+        this.setState({
+            movieToDeleteId: null
+        });
     }
 
     renderMovie(movie) {
@@ -79,7 +100,7 @@ class App extends Component {
                 <div className="title">
                     <h3>
                         <a href="#">{movie.title}</a>
-                        <Button onClick={() => this.deleteMovie(movie.id)}><Glyphicon glyph="remove"/></Button>
+                        <Button onClick={() => this.confirmDeleteMovie(movie.id)}><Glyphicon glyph="remove"/></Button>
                         <Button><Glyphicon glyph="edit"/></Button>
                     </h3>
 
